fdae3d38b1ccacba03476d196742f768dd5b1fcf
Brian Aguirre <bpaguirre.93@gmail.com>
2023-09-06 09:13:31 -0400

    Initial Commit

---
+++ DiscDrawer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiscDrawer
+//
+//  Created by Aguirre, Brian P. on 9/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiscDrawer/DiscDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscDrawerApp.swift
+//  DiscDrawer
+//
+//  Created by Aguirre, Brian P. on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DiscDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
