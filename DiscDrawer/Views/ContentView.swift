86bd5df04ee0069734b238fba4b0b78e6f72df1e
Brian Aguirre <bpaguirre.93@gmail.com>
2023-09-12 12:31:28 -0400

    create animated LogoView and add it to ContentView stack

--- DiscDrawer/Views/ContentView.swift
+++ DiscDrawer/Views/ContentView.swift
@@ -28,6 +28,8 @@ struct ContentView: View {
     @State private var sortAsc = true
     @State private var sortItemIndex = 0
     
+    @State private var showingLogoView = true
+    
     // Basic
     
     // Computed sort descriptor to pass to FilteredList
@@ -43,66 +45,86 @@ struct ContentView: View {
     
     var body: some View {
         
-        // Navigation view
-        NavigationView {
+        // ZStack for LogoView
+        ZStack {
             
-            // Main VStack
-            VStack {
+            // Navigation view
+            NavigationView {
                 
-                // Sort and filter elements
-                HStack {
+                // Main VStack
+                VStack {
                     
-                    // Sort button
-                    Button {
-                        sortAsc.toggle()
-                    } label: {
-                        Image(systemName: "arrow.up.arrow.down")
-                    }
-                    
-                    // Filter picker
-                    Picker("Filter Field", selection: $sortItemIndex) {
-                        Text("Name").tag(0)
-                        Text("Manufacturer").tag(1)
-                    }
-                    .pickerStyle(.segmented)
-                }
-                .padding(.horizontal)
-                
-                // Disc list
-                FilteredList(viewMode: viewMode, sortDescriptor: sortDescriptor)
-                    .navigationTitle("DiscDrawer")
-                    .toolbar {
+                    // Sort and filter elements
+                    HStack {
                         
-                        // View mode button
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            Button {
-                                if viewMode == "list" {
-                                    viewMode = "grid"
-                                } else {
-                                    viewMode = "list"
+                        // Sort button
+                        Button {
+                            sortAsc.toggle()
+                        } label: {
+                            Image(systemName: "arrow.up.arrow.down")
+                        }
+                        
+                        // Filter picker
+                        Picker("Filter Field", selection: $sortItemIndex) {
+                            Text("Name").tag(0)
+                            Text("Manufacturer").tag(1)
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    .padding(.horizontal)
+                    
+                    // Disc list
+                    FilteredList(viewMode: viewMode, sortDescriptor: sortDescriptor)
+                        .toolbar {
+                            
+                            // View mode button
+                            ToolbarItem(placement: .navigationBarLeading) {
+                                Button {
+                                    if viewMode == "list" {
+                                        viewMode = "grid"
+                                    } else {
+                                        viewMode = "list"
+                                    }
+                                } label: {
+                                    Image(systemName: viewMode == "grid" ? "list.bullet" : "square.grid.2x2")
                                 }
-                            } label: {
-                                Image(systemName: viewMode == "grid" ? "list.bullet" : "square.grid.2x2")
+                            }
+                            
+                            // Add disc button
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button {
+                                    showingAddScreen.toggle()
+                                } label: {
+                                    Label("Add Disc", systemImage: "plus")
+                                }
                             }
                         }
-                        
-                        // Add disc button
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button {
-                                showingAddScreen.toggle()
-                            } label: {
-                                Label("Add Disc", systemImage: "plus")
+                        .sheet(isPresented: $showingAddScreen) {
+                            
+                            // Manually add navigation view here to avoid adding a second navigation view when passing a disc
+                            NavigationView {
+                                AddEditDiscView()
                             }
+                            .interactiveDismissDisabled()
                         }
-                    }
-                    .sheet(isPresented: $showingAddScreen) {
-                        
-                        // Manually add navigation view here to avoid adding a second navigation view when passing a disc
-                        NavigationView {
-                            AddEditDiscView()
-                        }
-                        .interactiveDismissDisabled()
-                    }
+                }
+                .navigationTitle("Disc Drawer")
+            }
+            
+            // Logo view
+            if showingLogoView {
+                LogoView()
+                    .zIndex(1) // Keep view on top while dismissing
+                    .transition(.move(edge: .top))
+            }
+        }
+        .onAppear {
+            
+            // Logo view dismiss animation
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                withAnimation(.easeIn(duration: 0.2)) {
+                    showingLogoView.toggle()
+                }
             }
         }
     }
---
+++ DiscDrawer/Views/LogoView.swift
@@ -0,0 +1,132 @@
+//
+//  LogoView.swift
+//  DiscDrawer
+//
+//  Created by Aguirre, Brian P. on 9/12/23.
+//
+
+// MARK: - Imported libraries
+
+import SwiftUI
+
+// MARK: - Main struct
+
+// This struct provides an animated logo view
+struct LogoView: View {
+    
+    // MARK: - Properties
+    
+    // State
+    
+    @State private var discOffsetY = -500.0
+    @State private var drawerOffsetY = 0.0
+    
+    @State private var redOffsetY = -500.0
+    @State private var yellowOffsetY = -500.0
+    @State private var blueOffsetY = -500.0
+    
+    // Basic
+    
+    let discWidthMultiplier = 0.37
+    let discOffsetYFinal = -75.0
+    let discDropDuration = 0.2
+    
+    // MARK: - Body view
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                
+                // Background
+                Color.mint
+                    .ignoresSafeArea()
+                    
+                // Discs group
+                Group {
+                    
+                    // Blue disc
+                    Image("blue-disc")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width * discWidthMultiplier)
+                        .offset(x: 45, y: blueOffsetY)
+                    
+                    // Yellow disc
+                    Image("yellow-disc")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width * discWidthMultiplier)
+                        .offset(x: 0, y: yellowOffsetY)
+                    
+                    // Red disc
+                    Image("red-disc")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width * discWidthMultiplier)
+                        .offset(x: -45, y: redOffsetY)
+                }
+                
+                // Drawer
+                Image("drawer")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geo.size.width * 0.7)
+                    .offset(x: 0, y: drawerOffsetY)
+            }
+            .onAppear {
+                
+                // Disc drop animation
+                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                    withAnimation(.linear(duration: discDropDuration)) {
+                        setAllDiscsOffsetY(discOffsetYFinal)
+                    }
+                }
+                
+                // Drawer + disc slight drop animation
+                DispatchQueue.main.asyncAfter(deadline: .now() + discDropDuration) {
+                    withAnimation(.easeOut) {
+                        drawerOffsetY = 50.0
+                        setAllDiscsOffsetY(0.0)
+                    }
+                }
+                
+                // Drawer + disc return animation
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                    withAnimation(.easeIn(duration: 0.16)) {
+                        drawerOffsetY = 0.0
+                        setAllDiscsOffsetY(discOffsetYFinal)
+                    }
+                }
+                
+                // Disc jump animation
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.76) {
+                    withAnimation(Animation.timingCurve(0.1, 1, 0.95, 1, duration: 0.2)) {
+                        redOffsetY = discOffsetYFinal - 50
+                        yellowOffsetY = discOffsetYFinal - 30
+                        blueOffsetY = discOffsetYFinal - 70
+                    }
+                }
+                
+                // Disc return animation
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.96) {
+                    withAnimation(Animation.timingCurve(0.9, 0.05, 0.95, 1, duration: 0.2)) {
+                        setAllDiscsOffsetY(discOffsetYFinal)
+                    }
+                }
+            }
+        }
+    }
+    
+    // Helper func to set all disc Y offsets at once
+    func setAllDiscsOffsetY(_ offsetY: CGFloat) {
+        redOffsetY = offsetY
+        yellowOffsetY = offsetY
+        blueOffsetY = offsetY
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+    }
+}
